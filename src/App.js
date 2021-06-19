c2cdc8b45eff5156a541871f214253fe31795f14
Mercy Chege <mercychege699@gmail.com>
2021-06-19 11:41:56 +0300

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
+import React from "react";
 import './App.css';
+import Header from './Header.js';
+import Home from './Home.js';
+import Checkout from './Checkout.js';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    // BEM convention
+    <Router>      
+      <div className="app">
+        <Header />
+        <Switch>
+        <Route path="/login">            
+            <p> This is the login page</p>
+          </Route>
+          <Route path="/checkout">            
+            <Checkout />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+          
+        </Switch>
+        
+      </div>
+    </Router>
   );
 }
 
